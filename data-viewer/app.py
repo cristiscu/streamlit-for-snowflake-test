1183398e6b361db8dc1296d2aba138ddf4cc64c5
cristiscu <cristiscu@gmail.com>
2023-12-21 08:58:59 -0800

    email

--- data-viewer/app.py
+++ data-viewer/app.py
@@ -37,8 +37,7 @@ with st.sidebar:
     parent = st.selectbox("Parent Column Name", cols, index=1)
     df = df_orig[[child, parent]]
 
-    #st.sidebar.markdown(f"*User*: {st.experimental_user.email}")
-    st.sidebar.markdown(f"*User*: myemail@gmail.com")
+    st.sidebar.markdown(f"User: {st.experimental_user.email}")
 
 tabSource, tabFormat, tabGraph, tabChart, tabAnim = st.tabs(
     ["Source", "Format", "Graph", "Chart", "Animated"])
