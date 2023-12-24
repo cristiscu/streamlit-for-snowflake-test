6323588780258e56ad98029ae650abf29791f885
cristiscu <cristiscu@gmail.com>
2023-12-23 23:40:59 -0800

    update

--- data-viewer-snowflake/modules/utils.py
+++ data-viewer-snowflake/modules/utils.py
@@ -13,8 +13,8 @@ def getFullPath(filename):
     return f"{pardir}/{filename}"
 
 @st.cache_data(show_spinner="Running a Snowflake query...")
-def getDataFrame(session, query):
-    rows = session.sql(query).collect()
+def getDataFrame(_session, query):
+    rows = _session.sql(query).collect()
     return pd.DataFrame(rows).convert_dtypes()
 
 @st.cache_resource(show_spinner="Connecting to Snowflake...", max_entries=10)
